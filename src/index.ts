48b16370b7ac66b949132885ab6e6ca7b307ca17
memetrollsXD <memetrollsxd@gmail.com>
2022-10-13 09:47:27 +0200

    Change the log message to match with config

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.post('/upload', (req, res) => upload(req, res));
 app.get('/c/:content', (req, res) => displayAPI(req, res));
 app.get('/:content/:full', (req, res) => res.redirect(`/${req.params.content}`));
 app.get('/*', (req, res) => displayFriendly(req, res));
-app.listen(Config.HTTP_PORT, Config.HTTP_HOST, () => { console.log(`Listening on port 8008`) });
+app.listen(Config.HTTP_PORT, Config.HTTP_HOST, () => { console.log(`Listening on port ${Config.HTTP_PORT}`) });
 
 // Check if uploads/logs folder exists
 

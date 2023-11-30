5685013b7b4b654976b265a4b9eab2b3ad7220be
memetrollsXD <memetrollsxd@gmail.com>
2023-11-30 19:05:17 +0100

    layout idk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,10 @@
-balls.
+<p>crep mo</p>
 <slot/>
+
+<style>
+    :global(body) {
+        background-color: #1B263B;
+        color: white;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
